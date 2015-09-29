44c5f6352046b326209c9df7684f2758d020bd5d
camturko <camturkowski@gmail.com>
2015-09-29 20:32:25 +0000

    added welcome.sh

---
+++ welcome.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Welcome script
+myname="Cameron"
+mytitle="President"
+hostname=$(hostname)
+weekday=$(date +%A)
+echo "Welcome to planet" $hostname"," $mytitle $myname"! Today is" $weekday.
